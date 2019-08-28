2cdcc55abd19240b0bf69a52c642480cb2b4852a
ruslanchek <rshashkov@icloud.com>
2019-08-28 23:52:24 +0300

    Upd

--- src/pages/data.tsx
+++ src/pages/data.tsx
@@ -3,17 +3,31 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = props => (
-  <Layout>
-    <SEO title="Page two" />
-    {props.pageResources.json.data.swapi.allFilms.map(film => (
-      <div key={film.id}>{film.title}</div>
-    ))}
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = props => {
+  let allFilms = []
+
+  if (
+    props &&
+    props.pageResources &&
+    props.pageResources.json &&
+    props.pageResources.json.data &&
+    props.pageResources.json.data.swapi
+  ) {
+    allFilms = props.pageResources.json.data.swapi.allFilms || []
+  }
+
+  return (
+    <Layout>
+      <SEO title="Page two" />
+      {allFilms.map(film => (
+        <div key={film.id}>{film.title}</div>
+      ))}
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
 
 export default SecondPage
 
